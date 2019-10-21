1fa1ab109046f79a1b61428ef799f6d188bed847
rakhshanda-rashid <49167176+rakhshanda-rashid@users.noreply.github.com>
2019-10-22 00:29:02 +0200

    Delete index.js

--- react/week2/TodoList-App/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import ReactDOM from 'react-dom';
-import './index.css';
-import Title from "./Title"
-
-ReactDOM.render(<Title id='4'/>, document.getElementById('root'));
