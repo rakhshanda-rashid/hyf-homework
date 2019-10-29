ca9003d3c5b49db2db313500c1551e66acdf6de1
rakhshanda-rashid <49167176+rakhshanda-rashid@users.noreply.github.com>
2019-10-29 13:52:05 +0100

    Update index.js

--- react/week3/TodoApi/src/api/index.js
+++ react/week3/TodoApi/src/api/index.js
@@ -1,9 +1,9 @@
-const ROOT_URL = "https://gist.githubusercontent.com/benna100/391eee7a119b50bd2c5960ab51622532/raw";
+const URL = "https://gist.githubusercontent.com/benna100/391eee7a119b50bd2c5960ab51622532/raw";
 
 // NOTE: you don't need await if returning (response.json() is async)
-async function getList() {
-  const response = await fetch(`${ROOT_URL}`);
+async function todoListFromApi() {
+  const response = await fetch(`${URL}`);
   return response.json();
 }
 
-export { getList };
+export { todoListFromApi};
